98858855e52054b4979c6476b694bf226f5ec99d
teresa <teresasilver13@gmail.com>
2022-09-12 11:40:43 +0530

    first commit

---
+++ src/main/java/com/example/springbootproject/controllers/EnablePrometheusMetrics.java
@@ -0,0 +1,5 @@
+package com.example.springbootproject.controllers;
+
+public @interface EnablePrometheusMetrics {
+
+}
---
+++ src/main/java/com/example/springbootproject/controllers/TestController.java
@@ -0,0 +1,29 @@
+package com.example.springbootproject.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/message")
+
+public class TestController {
+    
+    @GetMapping("/hello")
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok("Hello all");
+    }
+    @GetMapping("/hi")
+    public ResponseEntity<String> get1(){
+        return ResponseEntity.ok("Hi all");
+    }
+   
+    
+    
+}
